c23b5f6ae283b61634515d0c636715080c9c4d34
mirsahib <mirsahib24@gmail.com>
2019-07-24 08:44:56 +0600

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
-
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -13,6 +12,6 @@ io.on("connection", function(socket) {
   });
 });
 
-http.listen(process.env.port || 3000, function() {
+http.listen(process.env.PORT || 3000, function() {
   console.log("listening on *:3000");
 });
